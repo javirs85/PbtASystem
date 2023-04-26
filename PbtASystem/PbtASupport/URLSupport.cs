05e26de50d4da697592a755aed10c7e72d3b95ef
JaviRS <javirs@gmail.com>
2023-04-26 12:58:23 +0200

    new

---
+++ PbtASystem/PbtASupport/URLSupport.cs
@@ -0,0 +1,14 @@
+﻿namespace PbtASystem.PbtASupport
+{
+	public static class URLSupport
+	{
+		public static bool IsRelease = false;
+		public static string URLAppendix{
+			get
+			{
+				if (IsRelease) return "/UrbanShadows/";
+				return "";
+			}
+		}
+	}
+}
